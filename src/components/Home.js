070c29d96eaf947e48c289847f6de2ba7a420545
puhuk <wetr235@gmail.com>
2019-06-14 23:25:49 +0900

    Page Routing

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/Cardcontent';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <Card>
+                <CardContent>
+                    React , Firebase 기반 React 앱
+                </CardContent>
+            </Card>
+        );
+    }
+}
+
+export default Home;
