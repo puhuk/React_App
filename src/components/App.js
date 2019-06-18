bb80dad4387dca1262f52e23d302b1e75809a8de
puhuk <wetr235@gmail.com>
2019-06-18 21:20:24 +0900

    Text detail, delete page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import AppShell from './AppShell';
-
 import Home from './Home';
 import Texts from './Texts';
 import Words from './Words';
+import Detail from './Details';
 
 class App extends React.Component {
     render() {
@@ -15,6 +15,7 @@ class App extends React.Component {
                         <Route exact path="/" component={Home}/>
                         <Route exact path="/texts" component={Texts}/>
                         <Route exact path="/words" component={Words}/>
+                        <Route exact path="/detail/:textID" component={Detail}/>
                     </div>
                 </AppShell>
             </Router>
